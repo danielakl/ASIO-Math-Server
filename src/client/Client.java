e869e604c57725528c54c4a10c5437bf241af055
Daniel Klock <danielkl@stud.ntnu.no>
2018-03-12 18:00:57 +0100

    Implement TLS server and client.

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,6 +3,13 @@ package client;
 import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLSocketFactory;
 
 public final class Client {
     private static final int PORT = 3000;
@@ -37,6 +44,35 @@ public final class Client {
         } catch (IOException ioe) {
             System.err.println("Error: sending packet over socket.");
             ioe.printStackTrace();
+        }
+    }
+
+    private static void tlsClient() {
+        /* From: http://java-buddy.blogspot.com/ */
+        SSLSocketFactory sslSocketFactory =
+                (SSLSocketFactory)SSLSocketFactory.getDefault();
+        try {
+            Socket socket = sslSocketFactory.createSocket("localhost", PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            try (BufferedReader bufferedReader =
+                         new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                Scanner scanner = new Scanner(System.in);
+                while(true){
+                    System.out.println("Enter something:");
+                    String inputLine = scanner.nextLine();
+                    if(inputLine.equals("q")){
+                        break;
+                    }
+
+                    out.println(inputLine);
+                    System.out.println(bufferedReader.readLine());
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
     }
 }
